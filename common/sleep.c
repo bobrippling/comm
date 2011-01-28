e976f7180b2d10c4cacc7a20e65fb525d92b0e36
Rob <robpilling@gmail.com>
2011-01-28 18:02:49 +0000

    Win32 compile fixes

--- common/sleep.c
+++ common/sleep.c
@@ -1,11 +1,19 @@
-#define _POSIX_C_SOURCE 199309L
-#include <time.h>
-#include "sleep.h"
+#ifdef _WIN32
+# include <winsock2.h>
+/* WTF */
+#else
+# define _POSIX_C_SOURCE 199309L
+# include <time.h>
+# include "sleep.h"
+#endif
 
 void sleep_ms(int ms)
 {
 #ifdef _WIN32
-	todo();
+	struct timeval tv;
+	tv.tv_usec = ms * 1000;
+	tv.tv_sec  = 0;
+	select(0, NULL, NULL, NULL, &tv);
 #else
 	struct timespec rqtp;
 
