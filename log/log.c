5c8f871ac2e72bfafbb58a562b9f5ed59d310d34
Rob Pilling <rob pilling at gmail>
2011-05-08 02:23:26 +0100

    Changed log file structure

--- log/log.c
+++ log/log.c
@@ -14,38 +14,58 @@
 #include "log.h"
 #include "../config.h"
 
-static char fname[32 + sizeof LOG_DIR];
+static char fname[2048 + sizeof LOG_DIR];
+
+int mkdir2(const char *path)
+{
+#ifdef _WIN32
+	if(!CreateDirectory(path, NULL) &&
+			/* success = ~0 ... sigh */
+			GetLastError() != ERROR_ALREADY_EXISTS)
+#else
+	if(mkdir(path, 0755) && errno != EEXIST)
+#endif
+		return 1;
+
+	return 0;
+}
+
+int mkdir_p(char *path)
+{
+	char *slash;
+
+	for(slash = strchr(path, '/');
+			slash;
+			slash = strchr(slash + 1, '/')){
+
+		*slash = '\0';
+		if(mkdir2(path))
+			return 1;
+		*slash = '/';
+	}
+
+	return 0;
+}
 
 int log_init(void)
 {
 	/* 2010-09-23.txt */
 	struct tm *tm;
 	time_t t;
+	FILE *f;
 
 	t = time(NULL);
 	if(!(tm = localtime(&t)))
 		return 1;
 
-	strftime(fname, sizeof fname,
-			LOG_DIR "/%Y-%m-%d.txt", tm);
+	strftime(fname, sizeof fname, LOG_DIR "/%Y-%m-%d/%H.%M.%S.txt", tm);
 
-
-#ifdef _WIN32
-	if(!CreateDirectory(LOG_DIR, NULL) &&
-			/* success = ~0 ... sigh */
-			GetLastError() != ERROR_ALREADY_EXISTS)
-#else
-	if(mkdir(LOG_DIR, 0755) && errno != EEXIST)
-#endif
+	if(mkdir_p(fname))
 		return 1;
 
-	{
-		char buf[16];
-		strftime(buf, sizeof buf,
-				"New Log %H:%M", tm);
-
-		log_add(buf);
-	}
+	f = fopen(fname, "a");
+	if(f)
+		fclose(f);
 
 	return 0;
 }
